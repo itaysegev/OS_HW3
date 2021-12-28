edef30a2c67918d38040b742eeae6c377f5d1215
Itay Segev <itaysegev11@gmail.com>
2021-12-28 18:25:55 +0200

    modify server.c for multi threads and diffrent policies

--- server.c
+++ server.c
@@ -1,5 +1,6 @@
 #include "segel.h"
 #include "request.h"
+#include "threadpool.h"
 
 // 
 // server.c: A very, very simple web server
@@ -12,40 +13,103 @@
 //
 
 // HW3: Parse the new arguments too
-void getargs(int *port, int argc, char *argv[])
+// void getargs(int *port, int argc, char *argv[])
+// {
+//     if (argc < 2) {
+// 	fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+// 	exit(1);
+//     }
+//     *port = atoi(argv[1]);
+// }
+void getargs(int *port, int *number_of_threads,int *max_requests, char **schedalg, int argc, char *argv[])
 {
-    if (argc < 2) {
-	fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-	exit(1);
+    if (argc < 5) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(1);
     }
     *port = atoi(argv[1]);
+    *number_of_threads = atoi(argv[2]);
+    *max_requests = atoi(argv[3]);
+    *schedalg = argv[4];
 }
-
 
 int main(int argc, char *argv[])
 {
-    int listenfd, connfd, port, clientlen;
+    // int listenfd, connfd, port, clientlen;
+    int listenfd, connfd, port, clientlen, number_of_threads, max_requests;
     struct sockaddr_in clientaddr;
-
-    getargs(&port, argc, argv);
-
+    char* schedalg = NULL; 
+    // getargs(&port, argc, argv);
+    getargs(&port, &number_of_threads, &max_requests, &schedalg, argc, argv);
+    thread_pool* thread_pool = create_new_pool(number_of_threads);
     // 
     // HW3: Create some threads...
     //
 
     listenfd = Open_listenfd(port);
+    // while (1) {
+	//     clientlen = sizeof(clientaddr);
+	//     connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+    //     
+        
+	// // 
+	// // HW3: In general, don't handle the request in the main thread.
+	// // Save the relevant info in a buffer and have one of the worker threads 
+	// // do the work. 
+	// // 
+	// requestHandle(connfd);
+
+	// Close(connfd);
+    // }
     while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
-
-	// 
-	// HW3: In general, don't handle the request in the main thread.
-	// Save the relevant info in a buffer and have one of the worker threads 
-	// do the work. 
-	// 
-	requestHandle(connfd);
-
-	Close(connfd);
+	    clientlen = sizeof(clientaddr);
+	    connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen); // same as the segel's code
+        struct timeval current_time;
+        gettimeofday(&current_time, NULL);
+	    if (strcmp(schedalg,"block") == 0) { // blocking policy
+	        pthread_mutex_lock(&(thread_pool->mutex));
+            // not enough buffers are available test 
+            if(thread_pool->waiting_tasks_queue->queue_size + thread_pool->current_task_counter >= max_requests) {
+                while (thread_pool->waiting_tasks_queue->queue_size + thread_pool->current_task_counter >= max_requests) {
+                    pthread_cond_wait(&(thread_pool->cond), &(thread_pool->mutex)); //wait until buffer become available
+                }
+                enqueue(connfd, current_time, thread_pool->waiting_tasks_queue);//continue as usual
+            }
+            else {
+                enqueue(connfd, current_time, thread_pool->waiting_tasks_queue); // if buffer is available continue as usual
+            }
+            pthread_cond_signal(&(thread_pool->cond)); // same as all policies 
+            pthread_mutex_unlock(&(thread_pool->mutex));
+	    }
+	    else if(strcmp(schedalg,"dt") == 0) {
+	        pthread_mutex_lock(&(thread_pool->mutex));
+	        if(thread_pool->waiting_tasks_queue->queue_size + thread_pool->current_task_counter >= max_requests) {
+                    Close(connfd); // drop the new request by closing the socket and continue listening
+            }
+	        else {
+                enqueue(connfd, current_time, thread_pool->waiting_tasks_queue); // if buffer is available same as all policies 
+	        }
+            pthread_cond_signal(&(thread_pool->cond));
+            pthread_mutex_unlock(&(thread_pool->mutex));
+	    }
+	    else if(strcmp(schedalg,"dh") == 0) {
+            pthread_mutex_lock(&(thread_pool->mutex));
+            if (thread_pool->waiting_tasks_queue->queue_size + thread_pool->current_task_counter >= max_requests) {
+                node *oldest_request = dequeue(thread_pool->waiting_tasks_queue); // queue for waiting requests only
+                if (oldest_request == NULL) { // queue is empty so moving to dt policy
+                    Close(connfd); 
+                }
+                else {
+                    Close(oldest_request->data);// drop the oldest request
+                    enqueue(connfd, current_time, thread_pool->waiting_tasks_queue); // add the new request 
+                }
+            }
+            else {
+                enqueue(connfd, current_time, thread_pool->waiting_tasks_queue);//same as all policies
+            }
+            pthread_cond_signal(&(thread_pool->cond));
+            pthread_mutex_unlock(&(thread_pool->mutex));
+        }
     }
 
 }
