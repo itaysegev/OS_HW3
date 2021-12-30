faae72393742f18406c780d1effd34f2ce4c4ef9
Tom Smolin <tomsmo016@gmail.com>
2021-12-30 15:44:32 +0200

    random addition, and some minor changes for compilation

--- queue.h
+++ queue.h
@@ -8,18 +8,19 @@ typedef struct node_t
     int data;
     struct timeval req_arrival;
     struct node_t* next;
-} node;
+} Node;
 
 typedef struct queue_t
 {
     int queue_size;
-    node* head;
-    node* tail;
-} queue;
+    Node* head;
+    Node* tail;
+} Queue;
 
-void enqueue(int connfd, struct timeval current_time, queue* queue);
-node* dequeue(queue* queue);
+void enqueue(int connfd, struct timeval current_time, Queue* queue);
+Node* dequeue(Queue* queue);
 //int removeFromQueue(Queue* queue, int index);
-int deleteNode(queue* queue, int position);
+int deleteNode(Queue* queue, int position);
 queue* createQueue();
+void dropRandomNodes(Queue* queue);
 #endif
--- threadpool.c
+++ threadpool.c
@@ -4,35 +4,37 @@
 
 typedef struct arg_struct
 {
-    thread_pool* thread_pool;
+    ThreadPool* thread_pool;
     int thread_id;
 } handle_func_args; // arguments for 
 
 
-static thread* createThreadsArr(int num_of_threads) {
+static thread* createThreadsArr(int num_of_threads, ThreadPool* thread_pool) {
     thread* new_arr = (thread*)malloc(num_of_threads * sizeof(thread)); 
     for(int i = 0 ; i < num_of_threads ; i++) {
         new_arr[i].thread_id = i;
         handle_func_args* args = (handle_func_args*) malloc(sizeof(handle_func_args));
         args->thread_pool = thread_pool;
         args->thread_id = i;
-        pthread_create(&(thread_pool->threads[i].thread), NULL ,thread_handle_request, (void*)handle_func_args);
+        pthread_create(&(thread_pool->threads[i].thread), NULL ,thread_handle_request, (void*)args);
     }
-    return new_arr
+    return new_arr;
 }
 
-thread_pool* create_new_pool(int num_of_threads) {
-    thread_pool* thread_pool = (thread_pool*) malloc(sizeof(thread_pool));
+ThreadPool* create_new_pool(int num_of_threads) {
+    ThreadPool* thread_pool = (ThreadPool*) malloc(sizeof(*thread_pool));
     thread_pool->num_of_threads = num_of_threads;
     thread_pool->waiting_tasks_queue = createQueue();
-    thread_pool->threads = createThreadsArr(num_of_threads); 
+    thread_pool->threads = createThreadsArr(num_of_threads, thread_pool);
+    pthread_mutex_init(&(thread_pool->mutex), NULL);
+    pthread_cond_init(&(thread_pool->cond), NULL);
     return thread_pool;
 }
 
 
 void* thread_handle_request(void* args) {
     handle_func_args* curr_args = (handle_func_args*) args;
-    thread_pool* thread_pool = curr_args->thread_pool;
+    ThreadPool* thread_pool = curr_args->thread_pool;
     int thread_id = curr_args->thread_id;
     while(1)
     {
@@ -50,7 +52,7 @@ void* thread_handle_request(void* args) {
         struct timeval req_dispatch = current_time;
         //requestHandle(curr_task->value, curr_task->req_arrival, req_dispatch, thread_id, thread_pool);
         //this line is for advanced implementation, for request handle.
-        requestHandle(curr_task->data) // request.h for more info
+        requestHandle(curr_task->data); // request.h for more info
         Close(curr_task->data); // same as server.c 
         //update number of current tasks
         pthread_mutex_lock(&(thread_pool->mutex)); // critical code
